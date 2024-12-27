f30c81f88bb28802508d2646f591cf02ee67133a
Krishna Vanstraelen <krishna.vanstraelen@nikonlenswear.com>
2024-12-27 10:53:59 +0900

    first workable simple F->C

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!!");
+    println!("Please input temperature to convert");
+        let mut temp = String::new();
+    
+        io::stdin()
+            .read_line(&mut temp)
+            .expect("Failed to read line");
+    
+        let temp: f32 =  temp
+            .trim()
+            .parse()
+            .expect("not a number"); //match {
+            //     Ok(num) => num,
+            //     Err(_) => continue,
+            // };
+    
+        println!("You want to convert: {}F", temp);
+
+        let new_temp = (temp - 32.0)*(5.0/9.0);
+
+        println!("which is equal to {}C", (new_temp*10.0).round()/10.0)
 }
